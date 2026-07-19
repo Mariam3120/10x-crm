04a85d43e31ae1b96719ba648b1a43acfe61425e
Elēctus <mariamgrigolia20@gmail.com>
2026-07-19 16:06:13 +0400

    feat: add dashboard html

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,13 +1,1 @@
-import { requireAuth } from "./guard.js";
-import { initTheme } from "./theme.js";
-import { initNav } from "./nav.js";
-
-function initDashboardPage() {
-  initTheme();
-  initNav();   // logout + active link now live here
-}
-
-if (requireAuth()) {
-  initDashboardPage();
-}
 
